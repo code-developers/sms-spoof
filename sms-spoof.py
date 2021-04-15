d4963858890d422632b6ff18dbefaaf1e2c0b727
krishpranav <krisna.pranav@gmail.com>
2021-04-15 16:16:01 +0530

    sms-spoof main function added

--- sms-spoof.py
+++ sms-spoof.py
@@ -3,10 +3,33 @@ import os
 import random
 import base64
 
-def cls():
+def clear():
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
 
-cls()
+clear()
+print("This Tool Is Used To Send Anonyous Messages\n\n")
+print("Enter The Details Of The Person You Want To Send Anonymous Message")
+cc = input("\tEnter Country Code (Without +) : ")
+if '+' in cc:
+        tc = list(cc)
+        tc.remove('+')
+        cc = ''.join(tc)
+        cc = cc.strip()
+if len(cc) >= 4 or len(cc) < 1:
+        print('\n\nInvalid Country Code..\n\t\tCountry Codes Are Generally 1-3 digits...\n')
+        exit()
+pn = input("Enter Phone Number(Without Country Code) : ")
+if len(pn) <= 6:
+        print('\n\nInvalid Phone Number..\n')
+        banner()
+        exit()
+number = cc + pn
+receiver = '+' + number
+text = input("Enter Message to send : ")
+os.system('''proxychains curl --request POST https://textbelt.com/text \
+    --data-urlencode phone='{}' \
+    --data-urlencode message='{}' \
+    -d key=textbelt'''.format(number,text))
